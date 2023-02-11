7614b0f19d9589ac501155f87961f69076285900
Wali-Muhammad123 <renedescartian@gmail.com>
2023-02-11 16:31:51 +0500

    Configured urls.py and settings.py

--- multivendor/multivendor/settings.py
+++ multivendor/multivendor/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+#import django rest framework
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainapp',
+    'api',
+    'rest_framework'
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
